0f7808e98398fc90274f2ff7d57550165277d73c
Odai Mohammad <omohammad@accumedsy.ae>
2021-06-23 14:06:13 +0300

    Setup system run

---
+++ run.sh
@@ -0,0 +1,13 @@
+#echo "Taking down services in case they exist..."
+#docker-compose down
+
+echo "Building projects..."
+./mvnw clean install -DskipTests
+
+echo "Spinning up services..."
+docker-compose up -d --build
+
+#echo "Waiting for Kafka Connect to be ready..."
+#sleep 60
+#echo "Creating MongoDb connector..."
+#curl -X POST -H "Content-Type: application/json" --data @kafka-connect-mongodb-source-config.json localhost:28082/connectors
