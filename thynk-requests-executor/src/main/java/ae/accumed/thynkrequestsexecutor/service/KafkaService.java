0cfa59fd1f9590f160c969964600e7008d18d37b
Odai Mohammad <omohammad@accumedsy.ae>
2021-08-12 15:05:54 +0300

    Fix kafka service issues

--- thynk-requests-executor/src/main/java/ae/accumed/thynkrequestsexecutor/service/KafkaService.java
+++ thynk-requests-executor/src/main/java/ae/accumed/thynkrequestsexecutor/service/KafkaService.java
@@ -6,7 +6,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaService {
     private final KafkaTemplate<String, JsonNode> kafkaTemplate;
 
